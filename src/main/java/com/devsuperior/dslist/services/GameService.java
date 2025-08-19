111685c4d36371042f3500a09e45dbee4ebf9ee2
Matheus Gonçalves <matheusgon503@gmail.com>
2025-08-18 21:26:17 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+import java.util.List;
+
+@Service
+public class GameService { // retorna o DTO
+
+    @Autowired
+    private GameRepository gameRepository; // injeta uma instância do GameRepository no GameService, seguindo as camadas REST
+
+    // gera uma consulta dos games no banco de dados e a converte em uma lista de games
+    public List<GameMinDTO> findALL() {
+        List<Game> result = gameRepository.findAll();
+        //transforma uma lista de Games, com todos os dados, para uma lista de GameMinDTO, onde há apenas os dados do DTO
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); 
+        return dto;
+    }
+}
